30448502619527bd8e9ff8fc52a1df33b4280951
Eric <ericaxt04@gmail.com>
2023-04-05 12:15:01 +0200

    Scaling of player and comments

--- SpaceBoom/GameScene.swift
+++ SpaceBoom/GameScene.swift
@@ -8,19 +8,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    let screenSize: CGRect = UIScreen.main.bounds
-    let player = SKSpriteNode(imageNamed: "player")
+    let player = SKSpriteNode(imageNamed: "player") //creates player node
     
     override func didMove(to view: SKView) {
-        if let particles = SKEffectNode(fileNamed: "Starfield"){
-            particles.position = CGPoint(x: 0, y: screenSize.maxY-150)
-            particles.zPosition = -1
+        if let particles = SKEffectNode(fileNamed: "Starfield"){ //adds particles of starfield to screen
+            particles.position = CGPoint(x: 0, y: frame.maxY + 500)
+            particles.zPosition = -1 // make particles appear in background layer
             addChild(particles)
         }
         
         player.name = "player"
-        player.position.y = screenSize.minY-150
-        player.zPosition = 1
-        addChild(player)
+        player.position.y = frame.minY + 25
+        player.zPosition = 1 // makes sure player visual is above the background layer
+        addChild(player) //adds player to screen
+        player.scale(to: CGSize(width: 40, height: 60)) // player size
     }
 }
